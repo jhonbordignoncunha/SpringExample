8c29fa24e1b6364aed5f6b0955c28ae3c501e39e
Jonathan Bordignon da Cunha <pet0015388@aluno.firjansenaisesi.com.br>
2026-04-13 14:20:30 -0300

    repository

---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/repositories/Pautarerpositories.java
@@ -0,0 +1,19 @@
+package com.senai.backend.controle_frequencia.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senai.backend.controle_frequencia.models.pauta;
+import com.senai.backend.controle_frequencia.models.usuario;
+
+
+@Repository
+
+public interface Pautarerpositories extends JpaRepository <pauta, Integer>  {
+
+
+    
+ 
+    
+}
---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/repositories/Usuariorerpositories.java
@@ -0,0 +1,16 @@
+package com.senai.backend.controle_frequencia.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senai.backend.controle_frequencia.models.usuario;
+
+@Repository
+public interface Usuariorerpositories extends JpaRepository <usuario, Integer>  {
+
+
+    
+ 
+    
+}
